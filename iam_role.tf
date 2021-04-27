9a37f832a6660d7c7cd8d5b66d5f173cb54ba174
farrukh90 <farrukhsadykov@gmail.com>
2021-04-27 01:01:33 +0000

    added policy and role

--- iam_role.tf
+++ iam_role.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "terraform_admin" {
+resource "aws_iam_role" "terraform_admin3" {
 	name = "terraform_admin3"
 	assume_role_policy = <<EOF
 {
@@ -15,4 +15,31 @@ resource "aws_iam_role" "terraform_admin" {
 ]
 }
 EOF
+}
+
+
+
+resource "aws_iam_policy" "policy4" {
+  name = "test-policy4"
+  description = "A test policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+{
+  "Action": [
+  "ec2:Describe*"
+],
+  "Effect": "Allow",
+  "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role = aws_iam_role.terraform_admin3.name
+  policy_arn = aws_iam_policy.policy4.arn
 }
