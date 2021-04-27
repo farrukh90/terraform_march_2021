95fc54c103d9696b75b669e0c43ffcc213012818
farrukh90 <farrukhsadykov@gmail.com>
2021-04-27 00:38:43 +0000

    added membership

--- iam.tf
+++ iam.tf
@@ -5,3 +5,11 @@ resource "aws_iam_user" "tim" {
 resource "aws_iam_group" "developers" {
     name = "developers"
 }
+
+resource "aws_iam_group_membership" "developers_team" {
+  name = "developers-group-membership"
+  users = [
+    aws_iam_user.tim.name
+  ]
+  group = aws_iam_group.developers.name
+}
