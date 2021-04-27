d472b441c98e2dadcc5cd26aedb8b876b9a7fd9f
farrukh90 <farrukhsadykov@gmail.com>
2021-04-27 00:49:06 +0000

    moved into 2 files

--- iam.tf
+++ iam.tf
@@ -2,19 +2,6 @@ resource "aws_iam_user" "tim" {
    name = "tim"
 }
 
-resource "aws_iam_group" "developers" {
-    name = "developers"
-}
-
-resource "aws_iam_group_membership" "developers_team" {
-  name = "developers-group-membership"
-  users = [
-    aws_iam_user.tim.name
-  ]
-  group = aws_iam_group.developers.name
-}
-
-
 resource "aws_iam_user" "multiuser" {
 	name = each.key
 	for_each = toset([
