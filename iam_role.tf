948ecc02af72a9e140e9b281305141852e378caf
farrukh90 <farrukhsadykov@gmail.com>
2021-04-27 00:55:55 +0000

    added admin role

---
+++ iam_role.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "terraform_admin" {
+	name = "terraform_admin3"
+	assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Action": "sts:AssumeRole",
+"Principal": {
+"Service": "ec2.amazonaws.com"
+},
+"Effect": "Allow",
+"Sid": ""
+}
+]
+}
+EOF
+}
