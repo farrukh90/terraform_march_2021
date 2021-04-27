20ee64b4043d1aaa39510f690a2fdeb22f7d8907
farrukh90 <farrukhsadykov@gmail.com>
2021-04-27 01:50:55 +0000

    added key

--- key.tf
+++ key.tf
@@ -2,3 +2,8 @@ resource "aws_key_pair" "deployer" {
   key_name   = "terraform_machine"
   public_key =  file("~/.ssh/id_rsa.pub")
 }
+
+# resource "aws_key_pair" "deployer2" {
+#   key_name   = "terraform_machine2"
+#   public_key =  file("~/.ssh/id_rsa.pub")
+# }
