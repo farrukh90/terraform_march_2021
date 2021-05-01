f5cb4886cbebd2d9040238504a7ab574be962de1
farrukh90 <farrukhsadykov@gmail.com>
2021-05-01 17:28:27 +0000

    added az

--- Class2/Terraform EC2/ec2.tf
+++ Class2/Terraform EC2/ec2.tf
@@ -2,4 +2,5 @@ resource "aws_instance" "web" {
   ami           = "ami-048f6ed62451373d9"
   instance_type = "t3.micro"
   key_name      = aws_key_pair.deployer.key_name
+  availability_zone = "us-east-1f"
 }
