fd85396765245abd073b9e5f2aa3143f0a1294a7
farrukh90 <farrukhsadykov@gmail.com>
2021-05-06 01:55:12 +0000

    added data folder

--- Class3/Old/ec2.tf
+++ Class3/Old/ec2.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-048f6ed62451373d9"
   instance_type = "t3.micro"
   user_data     = file("userdata")
+  tags = {
+    Name = "DevOps"
+    Team = "IT"
+  }
 }
--- Class3/Old/output.tf
+++ Class3/Old/output.tf
@@ -16,3 +16,18 @@ output "Public_ip" {
 output "arn" {
   value = aws_instance.web.arn
 }
+
+output  "DNS" {
+  value = aws_route53_record.www.zone_id
+}
+
+
+
+output "ami" {
+  value = aws_instance.web.ami
+}
+
+
+output "availability_zone" {
+  value = aws_instance.web.availability_zone
+}
