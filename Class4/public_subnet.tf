e40028b8eb49b5fef8fe1af3b60037700daae225
farrukh90 <farrukhsadykov@gmail.com>
2021-05-10 23:50:50 +0000

    added multiple subnets

--- Class4/public_subnet.tf
+++ Class4/public_subnet.tf
@@ -3,3 +3,13 @@ resource "aws_subnet" "public_subnet1" {
   cidr_block = var.public_cidr1
   tags = var.tags
 }
+resource "aws_subnet" "public_subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_cidr2
+  tags = var.tags
+}
+resource "aws_subnet" "public_subnet3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_cidr3
+  tags = var.tags
+}
