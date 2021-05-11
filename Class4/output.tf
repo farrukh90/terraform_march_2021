6e594d0bec8b86423343bc333d4188c54a1a4b3c
farrukh90 <farrukhsadykov@gmail.com>
2021-05-11 00:15:49 +0000

    added output

--- Class4/output.tf
+++ Class4/output.tf
@@ -4,3 +4,21 @@ output region {
 output "AZ" {
 	value = data.aws_availability_zones.available.names[0]
 }
+
+output "vpc" {
+	value = aws_vpc.main.id
+}
+output "public_subnets" {
+	value = [
+		aws_subnet.public_subnet1.id,
+		aws_subnet.public_subnet2.id,
+		aws_subnet.public_subnet3.id,
+	]
+}
+output "private_subnets" {
+	value = [
+		aws_subnet.private_subnet1.id,
+		aws_subnet.private_subnet2.id,
+		aws_subnet.private_subnet3.id,
+	]
+}
