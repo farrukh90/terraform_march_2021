23ef397ab4abb225d4427d456c645725763bdda6
farrukh90 <farrukhsadykov@gmail.com>
2021-05-15 16:38:37 +0000

    added lb

---
+++ Class5/lb.tf
@@ -0,0 +1,24 @@
+data "aws_availability_zones" "all" {}
+
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elbs"
+  availability_zones = data.aws_availability_zones.all.names
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
