e9d74ca3571ab3d746c30e73c9f5ed2a77a4d355
farrukh90 <farrukhsadykov@gmail.com>
2021-05-20 00:46:37 +0000

    added sec group

--- Class6/sec_group.tf
+++ Class6/sec_group.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "db" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_hosts]
+    cidr_blocks = var.allowed_hosts
   }
   egress {
     from_port   = 0
