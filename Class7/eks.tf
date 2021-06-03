509f5141bd9531a785e674ef8f3e53bd8e89ae14
farrukh90 <farrukhsadykov@gmail.com>
2021-06-03 00:48:53 +0000

    added data and eks

--- Class7/eks.tf
+++ Class7/eks.tf
@@ -1,14 +1,3 @@
-data "aws_eks_cluster" "cluster" {
-        name = module.my-cluster.cluster_id
-}
-data "aws_eks_cluster_auth" "cluster" {
-        name = module.my-cluster.cluster_id
-}
-provider "kubernetes" {
-        host = data.aws_eks_cluster.cluster.endpoint
-        cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-        token = data.aws_eks_cluster_auth.cluster.token
-}
 module "my-cluster" {
         source = "terraform-aws-modules/eks/aws"
         version = "12.0.0"
@@ -25,20 +14,3 @@ module "my-cluster" {
         ]
         tags = var.tags
 }
-
-output "cluster_id" {
-        value = module.my-cluster.cluster_id
-}
-output "cluster_arn" {
-        value = module.my-cluster.cluster_arn
-}
-output "cluster_version" {
-        value = module.my-cluster.cluster_version
-}
-output "cluster_security_group_id" {
-        value = module.my-cluster.cluster_security_group_id
-}
-output "workers_asg_names" {
-        value = module.my-cluster.workers_asg_names
-}
-
